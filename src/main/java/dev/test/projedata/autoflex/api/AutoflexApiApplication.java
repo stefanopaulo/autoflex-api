084a8d845d8185fc93c7b78eb72eef858d4b06ff
Stefano Paulo <stefanopaulo47@gmail.com>
2026-02-12 14:12:04 -0300

    start project

---
+++ src/main/java/dev/test/projedata/autoflex/api/AutoflexApiApplication.java
@@ -0,0 +1,13 @@
+package dev.test.projedata.autoflex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/test/projedata/autoflex/api/AutoflexApiApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.test.projedata.autoflex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AutoflexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
