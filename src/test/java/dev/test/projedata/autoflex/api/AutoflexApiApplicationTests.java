14c4826167072156184858b39d370d870ebc3083
Stefano Paulo <stefanopaulo47@gmail.com>
2026-02-14 11:26:52 -0300

    refactor: migrate database to PostgreSQL
    refactor: migrate database to PostgreSQL
    
    refactor: migrate database to PostgreSQL

--- src/test/java/dev/test/projedata/autoflex/api/AutoflexApiApplicationTests.java
+++ src/test/java/dev/test/projedata/autoflex/api/AutoflexApiApplicationTests.java
@@ -2,9 +2,7 @@ package dev.test.projedata.autoflex.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class AutoflexApiApplicationTests {
 
