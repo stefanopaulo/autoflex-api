cf6a8a01a049d772e4f1bd0988f8d96714f95ecd
Stefano Paulo <stefanopaulo47@gmail.com>
2026-02-15 18:47:09 -0300

    fix: adjustment cors configuration

--- src/main/java/dev/test/projedata/autoflex/api/configs/WebConfig.java
+++ src/main/java/dev/test/projedata/autoflex/api/configs/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("POST", "PUT", "PATCH", "GET", "DELETE")
-                .allowedHeaders("");
+                .allowedHeaders("*");
     }
 }
